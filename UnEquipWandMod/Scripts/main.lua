00e8da24eaae7319065346556c0d71b32f434e2f
nathtest <32673217+nathtest@users.noreply.github.com>
2023-02-15 00:14:46 +0100

    version 0.3

--- UnEquipWandMod/Scripts/main.lua
+++ UnEquipWandMod/Scripts/main.lua
@@ -1,20 +1,40 @@
 local BipedPlayer = nil
+local GearManager = nil
 
 function Init()
     BipedPlayer = FindFirstOf("Biped_Player")
-    -- print(string.format("BipedPlayer %s..\n", BipedPlayer:GetFullName()))
+    GearManager = FindFirstOf("GearManager")
+    IsInitialized = true
 end
 
 
+function PlayerGear()
+
+    if(GearManager:IsValid() and BipedPlayer:IsValid())
+    then
+
+        local GearItemID = GearManager.GetEquippedGearItemID(GearManager, BipedPlayer, 1)
+               
+        if(GearManager.IsHoodUp(GearManager, BipedPlayer))
+        then
+          GearManager.SetHoodPosition(GearManager, BipedPlayer, GearItemID, false, true) 
+        else
+          GearManager.SetHoodPosition(GearManager, BipedPlayer, GearItemID, true, true) 
+        end
+    else
+        print("No instance of class 'GearManager' was found.")
+    end
+end
+
 function PlayerUnEquipWand()
 
-    if(BipedPlayer ~= nil)
+    if(BipedPlayer:IsValid())
     then
         BipedPlayer.SetPartialBodyState(17)
         BipedPlayer.UnEquipWand()
         BipedPlayer.SetPartialBodyState(0)
     else
-        print("BipedPlayer is nil\n")
+        print("No instance of class 'Biped_Player' was found.")
     end
 end
 
@@ -22,7 +42,14 @@ end
 RegisterKeyBind(Key.F6, function()
     ExecuteInGameThread(function()
         Init()
-        -- print("PlayerUnEquipWand\n")
         PlayerUnEquipWand()	
+    end)
+end)
+
+
+RegisterKeyBind(Key.F7, function()
+    ExecuteInGameThread(function()
+        Init()
+        PlayerGear()	
     end)
 end)
